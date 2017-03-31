1d811ca90c8f3c1a8ac6a51bb77430178ec72278
Jameson Hill <stevenjamesonhill@gmail.com>
2017-03-31 15:51:16 -0400

    added recipes deletion

--- server.js
+++ server.js
@@ -77,6 +77,12 @@ app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
 
+app.delete('/recipes/:id', (req, res){
+  Recipes.delete(req.params.id);
+  console.log(`deleted recipe |`${req.params.id}\``);
+  res.status(204).end();
+});
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
